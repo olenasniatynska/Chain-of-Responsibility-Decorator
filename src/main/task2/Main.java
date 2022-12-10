a487867de81bb0152eedf8c9988f8ec34138d148
olenasniatynska <92575026+olenasniatynska@users.noreply.github.com>
2022-12-10 18:03:16 +0200

    Create Main.java

---
+++ src/main/task2/Main.java
@@ -0,0 +1,11 @@
+package task2;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Document document = new SmartDocument("gs://oop-course/Geico-2021.png");
+        System.out.println(document.parse());
+
+
+    }
+}
